3db48495e7a9e684cf7b0baed6f0683d33a6aebd
SubhPa19 <subhash.pandey1987@gmail.com>
2026-03-05 19:24:34 +0530

    Update SecondFragment.kt

--- app/src/main/java/com/example/geminidemo/SecondFragment.kt
+++ app/src/main/java/com/example/geminidemo/SecondFragment.kt
@@ -19,15 +19,7 @@ class SecondFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
-
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
